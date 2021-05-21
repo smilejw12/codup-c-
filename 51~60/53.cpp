7fcd602f6cd6d63f87403ca5fc70142f5e0e770f
smilejw12 <80508409+smilejw12@users.noreply.github.com>
2021-05-21 14:28:38 +0900

    51~60

---
+++ 51~60/53.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	bool x;
+	cin >> x;
+	if (x == 1) {
+		cout << 0 << endl;
+	}
+	else {
+		cout << 1 << endl;
+	}
+}
---
+++ 51~60/54.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	bool x, y;
+	cin >> x >> y;
+	if (x == 1 && y == 1) {
+		cout << 1 << endl;
+	}
+	else {
+		cout << 0 << endl;
+	}
+}
+
