13e6d76cd0cee967d19b15fa7bbd83fcc470600d
smilejw12 <80508409+smilejw12@users.noreply.github.com>
2021-05-21 14:39:19 +0900

    Update 46.cpp
    정확한 답으로 수정

--- 41~50/46.cpp
+++ 41~50/46.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-	int op1, op2, op3
-		cin >> op1 >> op2 >> op3;
+	int op1, op2, op3;
+	cin >> op1 >> op2 >> op3;
 	cout << op1 + op2 + op3 << endl;
 	float ave = (op1 + op2 + op3) / 3.0;
-	cout << setprecision(2) << result;
+	cout << fixed << setprecision(1) << ave;
 }
